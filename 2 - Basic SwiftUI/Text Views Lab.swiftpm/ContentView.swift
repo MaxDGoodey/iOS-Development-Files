df1b2d22b34382f2028cb5b4d20e09681498a3c5
Max Goodey <>
2026-09-11 13:52:57 -0600

    Stack views lab

--- 2 - Basic SwiftUI/Text Views Lab.swiftpm/ContentView.swift
+++ 2 - Basic SwiftUI/Text Views Lab.swiftpm/ContentView.swift
@@ -2,20 +2,37 @@ import SwiftUI
 
 struct AboutTheDeveloper: View {
     var body: some View {
-        Text("About the developer")
-            .font(.largeTitle)
-            .padding()
-            .bold()
-        Text("Max")
-            .font(.title)
-            .bold()
-        Text("I am 17 years old. I go to orem high, and I play a lot of piano. I love video games, and the objectively best video game is Outer Wilds")
-            .padding()
-            .foregroundStyle(.red)
-        Text("Fun Facts")
-            .font(.headline)
-            .bold()
-        Text("I have 3 pets. I can play clair de lune on the piano. I just beat the adventure of link.")
-            .padding()
+        ZStack {
+            Rectangle()
+                .fill(.green)
+                .ignoresSafeArea()
+            VStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.thinMaterial)
+                        .frame(width: 350, height: 100)
+                    Text("About the developer")
+                        .font(.largeTitle)
+                        .bold()
+                }
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Name:")
+                        .font(.largeTitle)
+                        .bold()
+                    Text("Max")
+                        .font(.title)
+                        .bold()
+                    Text("About Me:")
+                        .font(.largeTitle)
+                        .bold()
+                    Text("I am 17 years old. I go to orem high, and I play a lot of piano. I love video games, and the objectively best video game is Outer Wilds")
+                        .foregroundStyle(.red)
+                    Text("Fun Facts:")
+                        .font(.largeTitle)
+                        .bold()
+                    Text("I have 3 pets, I can play clair de lune on the piano, and I have 7 siblings.")
+                }
+            }
+        }
     }
 }
